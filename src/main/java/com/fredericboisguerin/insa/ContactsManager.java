6e3a02eb0cd26c7c62ff40adb41761171433b0db
gayouba <gael.ayouba@gmail.com>
2017-11-16 01:38:00 +0100

    TP1: fonctions de managecontact (manque exceptions)

--- src/main/java/com/fredericboisguerin/insa/ContactsManager.java
+++ src/main/java/com/fredericboisguerin/insa/ContactsManager.java
@@ -1,16 +1,34 @@
 package com.fredericboisguerin.insa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactsManager {
 
-    public void addContact(String name, String email, String phoneNumber) {
+    List<Contact> contactList = new ArrayList<>();
 
+    public void addContact(String name, String email, String phoneNumber) {
+        Contact contact1 = new Contact(name,email,phoneNumber);
+        contactList.add(contact1);
     }
 
     public void printAllContacts() {
 
+        for (int i=0; i<contactList.size();i++){
+            System.out.println(contactList.get(i).getName()+", "+contactList.get(i).getEmail()+", "+contactList.get(i).getPhoneNumber());
+        }
+
     }
 
     public void searchContactByName(String name) {
-
+        boolean found = false;
+        int i=0;
+        while (i<contactList.size()&&!found){
+            if (contactList.get(i).getName().toLowerCase().contains(name)){
+                found=true;
+                System.out.println(contactList.get(i).getName()+", "+contactList.get(i).getEmail()+", "+contactList.get(i).getPhoneNumber());
+            }
+            i++;
+        }
     }
 }
